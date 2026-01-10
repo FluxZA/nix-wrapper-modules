fcb73cf06a6a249adf67152d0690a2c72dfe88a6
Patrick Widmer <patrick.widmer@ergon.ch>
2026-01-10 18:06:21 +0100

    type(wrapperModules.waybar): init

---
+++ wrapperModules/w/waybar/check.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  self,
+}:
+let
+  waybarWrapped = self.wrapperModules.waybar.wrap {
+    inherit pkgs;
+
+    settings = {
+      position = "top";
+      modules-left = [ ];
+      modules-right = [ ];
+      modules-center = [ ];
+    };
+
+    "style.css".content = "";
+  };
+
+in
+if builtins.elem pkgs.stdenv.hostPlatform.system self.wrapperModules.mako.meta.platforms then
+  pkgs.runCommand "waybar-test" { } ''
+    "${waybarWrapped}/bin/waybar" --version | grep -q "${waybarWrapped.version}"
+    touch $out
+  ''
+else
+  pkgs.runCommand "waybar-test-disabled" { } ''
+    touch $out
+  ''
