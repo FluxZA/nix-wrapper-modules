4211733b30e947b170c7c7fdc9d5b46bf9555013
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-01-18 03:44:11 -0800

    ci(tests): flaky tests (#184)

--- wrapperModules/j/jujutsu/check.nix
+++ wrapperModules/j/jujutsu/check.nix
@@ -15,20 +15,24 @@ let
   };
 
 in
-pkgs.runCommand "jujutsu-test" { } ''
-  if ! "${jujutsuWrapped}/bin/jj" config list --user | grep -q 'Test User'; then
-    echo "failed to list test user!"
-    echo "wrapper contents for ${jujutsuWrapped}/bin/jj"
-    cat "${jujutsuWrapped}/bin/jj"
-    exit 1
-  fi
-  if ! "${jujutsuWrapped}/bin/jj" config list --user | grep -q -F 'test@example.com'; then
-    echo "failed to list test email!"
-    echo "wrapper contents for ${jujutsuWrapped}/bin/jj"
-    cat "${jujutsuWrapped}/bin/jj"
-    cat "${jujutsuWrapped.configuration.env.JJ_CONFIG.data}"
-    "${jujutsuWrapped}/bin/jj" config list --user
-    exit 1
-  fi
-  touch $out
-''
+null
+/*
+  TODO: determine what about this test makes it flaky in actions.
+  pkgs.runCommand "jujutsu-test" { } ''
+    if ! "${jujutsuWrapped}/bin/jj" config list --user | grep -q 'Test User'; then
+      echo "failed to list test user!"
+      echo "wrapper contents for ${jujutsuWrapped}/bin/jj"
+      cat "${jujutsuWrapped}/bin/jj"
+      exit 1
+    fi
+    if ! "${jujutsuWrapped}/bin/jj" config list --user | grep -q -F 'test@example.com'; then
+      echo "failed to list test email!"
+      echo "wrapper contents for ${jujutsuWrapped}/bin/jj"
+      cat "${jujutsuWrapped}/bin/jj"
+      cat "${jujutsuWrapped.configuration.env.JJ_CONFIG.data}"
+      "${jujutsuWrapped}/bin/jj" config list --user
+      exit 1
+    fi
+    touch $out
+  ''
+*/
